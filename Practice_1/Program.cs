fe76c754608e0d3b5aaa27af4d754b128a7842c6
Никита Лапан <nvlapan@edu.hse.ru>
2018-06-25 13:51:00 +0500

    1.0 full availability

--- Practice_1/Program.cs
+++ Practice_1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,35 @@ namespace Practice_1
     {
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("input.txt");
+            StreamWriter sw = new StreamWriter("output.txt");
+
+            string[] s = sr.ReadLine().Split();
+
+            int N = int.Parse(s[0]);
+            int A = int.Parse(s[1]);
+            int B = int.Parse(s[2]);
+
+            BigInteger r = c(N + 1, A) * c(N + 1, B);
+            sw.WriteLine(r);
+
+            sr.Close();
+            sw.Close();
+        }
+
+        static BigInteger c(int n, int k)
+        {
+            return f(n + k - 1) / (f(k) * f(n - 1));
+        }
+
+        static BigInteger f(int n)
+        {
+            BigInteger r = 1;
+
+            for (int i = 2; i <= n; i++)
+                r *= i;
+
+            return r;
         }
     }
 }
